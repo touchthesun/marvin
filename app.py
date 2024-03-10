ac393b89de85e02db6a81ee07e6974e9ba292559
Nathan <touchthesun@gmail.com>
2024-03-09 21:47:43 -0800

    working prototype

--- app.py
+++ app.py
@@ -12,9 +12,6 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 
 load_dotenv()
 
-def get_response(user_input):
-    return "I don't know"
-
 
 def get_vectorstore_from_url(url):
     # get the text in the documents
@@ -66,21 +63,17 @@ def get_response(user_input):
 
     response = conversation_rag_chain.invoke({
         "chat_history": st.session_state.chat_history,
-        "input": user_query
+        "input": user_input
     })
 
     return response['answer']
 
-    
+
 
 # app config
 st.set_page_config(page_title="Chat with websites")
 st.title("Chat with websites")
 
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = [
-    AIMessage(content="Hello, I am a bot. How can I help you?")
-]
 
 # sidebar
 with st.sidebar:
@@ -88,13 +81,18 @@ with st.sidebar:
     website_url = st.text_input("Website URL")
 
 
-if website_url is None or website_url == "":...
+if website_url is None or website_url == "":
+    st.info("Please enter a URL")
 
 else:
-    documents = get_vectorstore_from_url(website_url)
+    # session state
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello, I am a bot. How can I help you?")
+        ]
+    if "vector_store" not in st.session_state:
+        st.session_state.vector_store = get_vectorstore_from_url(website_url)
 
-    with st.sidebar:
-        st.write(documents)
 
     user_query = st.chat_input("Type your message here...")
     if user_query is not None and user_query != "":
@@ -102,9 +100,6 @@ else:
         st.session_state.chat_history.append(HumanMessage(content=user_query))
         st.session_state.chat_history.append(AIMessage(content=response))
 
-
-    with st.sidebar:
-        st.write(st.session_state.chat_history)
 
     # conversation
 
